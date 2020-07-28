5be49236194285221e907bbabefcf083c471de5b
Kuivalainen <54420379+DenisKuivalainen@users.noreply.github.com>
2020-07-28 16:29:48 +0300

    back update

---
+++ server/build/precache-manifest.e0a9b12b1e061fcf940a42e6273d94d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca974f8b91e7fbf43af2fe22e27cb2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfcbe59c4fa9ab92298c",
+    "url": "/static/css/main.ffb9d77f.chunk.css"
+  },
+  {
+    "revision": "fbeca296fcdb6317ed86",
+    "url": "/static/js/2.935a0d04.chunk.js"
+  },
+  {
+    "revision": "cfcbe59c4fa9ab92298c",
+    "url": "/static/js/main.9cd26556.chunk.js"
+  },
+  {
+    "revision": "839f7059ed47a81399ae",
+    "url": "/static/js/runtime-main.79308a23.js"
+  }
+]);
